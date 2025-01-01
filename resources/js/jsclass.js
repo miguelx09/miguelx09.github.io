fa319332e1f865c4b9f431e526863633069c5978
Miguelx09 <miguelcortes0909@gmail.com>
2025-01-01 12:43:32 +0000

    classificacao color

---
+++ resources/js/jsclass.js
@@ -0,0 +1,4 @@
+document.querySelector('.navbar-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('inverted');
+    document.querySelector('.header').classList.toggle('inverted');
+});
