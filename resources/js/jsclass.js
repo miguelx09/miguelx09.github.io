b7e1ee27683d05d580dd0f04df55c0ca693184fb
iurisilvaa78 <siuri1845@gmail.com>
2025-01-03 04:15:40 +0000

    add js switch colspan

--- resources/js/jsclass.js
+++ resources/js/jsclass.js
@@ -2,3 +2,18 @@ document.querySelector('.navbar-toggle').addEventListener('click', function () {
     document.body.classList.toggle('inverted');
     document.querySelector('.header').classList.toggle('inverted');
 });
+
+function ajustarColspan() {
+    const larguraJanela = window.innerWidth;
+    const legenda = document.querySelector('.titulo-clube');
+
+    if (larguraJanela <= 768) {
+       
+        legenda.setAttribute('colspan', '3');
+    } else {
+        
+        legenda.setAttribute('colspan', '2');
+    }
+}
+ajustarColspan();
+window.addEventListener('resize', ajustarColspan);
