68ce5b41563bba09601c04d529e1789b6157bf01
Jonybtw <cego04643@gmail.com>
2025-01-03 00:45:43 +0000

    HTML Fix + JS Fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,7 @@
-// Função para inverter as cores
 document.getElementById('invert-colors').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
 
-// Estilo adicional para dark mode
 const style = document.createElement('style');
 style.textContent = `
     .dark-mode {
@@ -27,16 +25,14 @@ document.head.appendChild(style);
 
 const scrollToTopButton = document.getElementById('scroll-to-top');
 
-// Mostrar ou esconder o botão ao rolar
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 200) { // Mostra o botão quando o scroll é maior que 200px
+    if (window.scrollY > 200) {
         scrollToTopButton.style.display = 'flex';
     } else {
         scrollToTopButton.style.display = 'none';
     }
 });
 
-// Scroll suave para o topo ao clicar no botão
 scrollToTopButton.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
