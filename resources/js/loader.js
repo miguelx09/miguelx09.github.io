22bf56a5bec69554df7e6c29363e3f39faff0499
Jonybtw <cego04643@gmail.com>
2025-01-04 04:03:10 +0000

    Dark Mode Update + Loader Update + HTML Update

--- resources/js/loader.js
+++ resources/js/loader.js
@@ -1,14 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var loader = document.getElementById("loader");
-    loader.style.display = "flex";
-});
-
 window.addEventListener("load", function() {
-    var loader = document.getElementById("loader");
-    setTimeout(function() {
-        loader.style.opacity = 0;
-        setTimeout(function() {
-            loader.style.display = "none";
-        }, 500);
-    }, 500);
+    var loader = document.querySelector(".loader");
+    var mainFader = document.querySelector(".main-fader");
+
+    mainFader.style.opacity = 0;
+
+    mainFader.addEventListener('transitionend', function() {
+        mainFader.style.display = 'none';
+    });
 });
