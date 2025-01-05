e39ec5c0463ed6b172bf7be5b349043aad455549
Jonybtw <cego04643@gmail.com>
2025-01-05 23:50:45 +0000

    Clubs Fix

--- resources/js/clubsinfo.js
+++ resources/js/clubsinfo.js
@@ -1,4 +1,3 @@
-// Dados dos clubes
 const clubs = {
     sporting: {
         name: "Sporting Clube de Portugal",
@@ -32,20 +31,20 @@ const clubs = {
             coach: "Roger Schmidt",
             titles: ["Primeira Liga", "Taça de Portugal", "Liga Europa"],
         }
-    }
+    },
 };
 
-// Aguarda o carregamento do DOM
+
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
-    const clubName = urlParams.get('club'); // Obtém o valor do parâmetro 'club'
+    const clubName = urlParams.get('club');
+    const clubInfoTitle = document.getElementsByClassName('nav-buttons')[0];
     const clubInfoContainer = document.getElementById('club-info');
 
-    // Se um clube específico for selecionado
     if (clubName && clubs[clubName]) {
         const club = clubs[clubName];
 
-        clubInfoContainer.innerHTML = `
+        clubInfoContainer.innerHTML += `
             <div class="club-detail-container">
                 <img src="${club.logo}" alt="${club.name}" class="club-logo">
                 <h1>${club.name}</h1>        
@@ -54,12 +53,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p><strong>Estádio:</strong> ${club.details.stadium}</p>
                 <p><strong>Treinador:</strong> ${club.details.coach}</p>
                 <p><strong>Títulos:</strong> ${club.details.titles.join(", ")}</p>
-                <a href="clubs.html" class="back-button">⬅️ Voltar para todos os clubes</a>
+                <a href="clubs.html" class="back-button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="m12.718 4.707-1.413-1.415L2.585 12l8.72 8.707 1.413-1.415L6.417 13H20v-2H6.416l6.302-6.293z"/></svg> Voltar para todos os clubes</a>
             </div>
         `;
     } else {
-        // Se nenhum clube for selecionado, mostra todos
-        clubInfoContainer.innerHTML = `<h1>Todos os Clubes</h1>`;
+        clubInfoTitle.outerHTML += `<h1>Todos os Clubes</h1>`;
         clubInfoContainer.classList.add("club-grid");
         Object.values(clubs).forEach((club, index) => {
             const clubKey = Object.keys(clubs)[index];
@@ -70,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <h3>${club.name}</h3>
                     <p>${club.description}</p>
                     <p><strong>Pontos:</strong> ${club.points}</p>
-                    <a href="clubs.html?club=${clubKey}" class="view-button">Ver Detalhes ➡️</a>
+                    <a href="clubs.html?club=${clubKey}" class="view-button">Ver Detalhes <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M11.293 4.707 17.586 11H4v2h13.586l-6.293 6.293 1.414 1.414L21.414 12l-8.707-8.707-1.414 1.414z"/></svg></a>
                 </div>
             `;
         });
